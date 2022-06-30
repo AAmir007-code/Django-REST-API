73beb9887d4f452e6d52e14604dd406e3f594a82
Amirbek <61391976+AAmir007-code@users.noreply.github.com>
2022-06-30 21:17:24 +0500

    Update settings.py

--- autosalon/autosalon/settings.py
+++ autosalon/autosalon/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n(pw$bk(olv0=+5r+j*+&93bgpofvlo%=$3$!foue1gz3qz!ad'
+SECRET_KEY = 'YOUR SECRET KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
